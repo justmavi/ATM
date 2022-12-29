6c6f79ae0e31333310173b9cc8438edd2359cae2
Grish <36304554+justmavi@users.noreply.github.com>
2022-12-29 14:10:26 +0400

    style: add whitespace

--- ATM/Exceptions/MaxAmountExceedException.cs
+++ ATM/Exceptions/MaxAmountExceedException.cs
@@ -5,6 +5,7 @@ namespace ATM.Exceptions
     internal class MaxAmountExceedException : ATMException
     {
         public int MaxAmount { get; }
+        
         public MaxAmountExceedException(int maxAmount) : base("Max amount exceeded. Max amount: " + maxAmount)
         {
             MaxAmount = maxAmount;
